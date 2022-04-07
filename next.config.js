ab8c1b9d63f711bea4620c7cf67c59439c6485e0
JimHuang <402279089@qq.com>
2022-04-08 00:13:21 +0800

    2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['en-US', 'zh-CN'],
+    defaultLocale: 'zh-CN',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
