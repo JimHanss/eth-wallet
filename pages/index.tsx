5cf776a72ee605874ce176b1f3c4e489df39f459
JimHuang <402279089@qq.com>
2022-05-19 21:21:53 +0800

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import { ethers } from 'ethers';
 import type { NextPage } from 'next';
+import { useTranslation } from 'next-i18next';
 import { useCallback, useEffect } from 'react';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const Home: NextPage = () => {
+  const [t, { language }] = useTranslation('block');
   // let provider = ethers.getDefaultProvider('homestead');
+
   let provider = new ethers.providers.JsonRpcProvider({ url: 'http://127.0.0.1:8545' });
   const getTest = useCallback(async () => {
     try {
@@ -18,9 +22,18 @@ const Home: NextPage = () => {
     getTest();
   }, [getTest]);
 
-  console.log('provider', provider);
+  // console.log('provider', provider);
+  // console.log('language', t);
 
-  return <div>ETH</div>;
+  return <div>{t('hello')}</div>;
+};
+
+export const getStaticProps = async ({ locale }: { locale: string }) => {
+  const lng = await serverSideTranslations(locale, ['block']);
+
+  return {
+    props: lng,
+  };
 };
 
 export default Home;
